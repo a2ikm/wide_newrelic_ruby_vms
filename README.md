3b944dcba29350454599f307106f6c1e4a224765
Masato Ikeda <masato.ikeda@gmail.com>
2017-02-09 13:29:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+WideNewRelicRubyVMs
+===================
---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+  var $mainColumn = $("#main_column");
+
+  var $helpPanel = $mainColumn.find(".help_panel").parent();
+  $helpPanel.remove();
+
+  $mainColumn.find(".span-8").removeClass("span-8").addClass("span-12");
+  $mainColumn.find(".span-4").removeClass("span-4").addClass("span-6");
+});
